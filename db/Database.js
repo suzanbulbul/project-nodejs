bdd0cd41edaf6d2e22f6b6c5b9b98a399f7543a2
Suzan Bülbül <suzanbulbl@gmail.com>
2023-11-16 15:09:17 +0300

    changed db-url

--- db/Database.js
+++ db/Database.js
@@ -6,7 +6,7 @@ class Database {
 
     constructor() {
         if (!instance) {
-            this.mongoConnection = null;
+            this.mongoConnection = null; 
             instance = this;
         }
 
@@ -15,11 +15,24 @@ class Database {
 
     async connect(options) {
         try {
-            console.log("DB Connecting...");
-            let db = await mongoose.connect(options.CONNECTION_STRING);
+            if(process.env.CONNECTION_STRING === "mongodb://127.0.0.1:27017/project-nodejs"){
+                console.log("DB Connecting...");
+                let db = await mongoose.connect(options.CONNECTION_STRING);
 
-            this.mongoConnection = db;
-            console.log("DB Connected.");
+                this.mongoConnection = db;
+                console.log("DB Connected.");
+            }
+            else{
+                mongoose.connect(options.CONNECTION_STRING, {
+                    dbName: 'test',
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true,
+                }).then(() => console.log('Live DB Connected!'))
+                  .catch(err => {
+                    console.log(`DB Connection Error: ${err.message}`);
+                  });
+            }
+           
         } catch (err) {
             console.error(err);
             process.exit(1);
